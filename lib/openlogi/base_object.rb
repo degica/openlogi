8989f4aa88f256ee2e6fc80a289ab5dd44056eba
Chris Salzberg <csalzberg@degica.com>
2016-02-19 18:01:06 +0900

    Use Virtus constructor in BaseObject

--- lib/openlogi/base_object.rb
+++ lib/openlogi/base_object.rb
@@ -1,11 +1,5 @@
 module Openlogi
   class BaseObject
     include Virtus.model
-
-    def initialize(options = {})
-      options.each do |key, value|
-        self.public_send("#{key}=", value)
-      end
-    end
   end
 end
