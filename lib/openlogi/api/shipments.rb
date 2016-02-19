9aa2bffb2c9f489fa6887c9259bd2e13c2b931cc
Chris Salzberg <csalzberg@degica.com>
2016-02-19 16:33:16 +0900

    Add basic actions for warehousings + shipments endpoints

--- lib/openlogi/api/shipments.rb
+++ lib/openlogi/api/shipments.rb
@@ -1,6 +1,25 @@
 module Openlogi
   module Api
     class Shipments < Endpoint
+      def find(id)
+        perform_request_with_object(:get, "shipments/#{id}", {}, Openlogi::Shipment)
+      end
+
+      def all
+        perform_request_with_objects(:get, "shipments", {}, Openlogi::Shipment)
+      end
+
+      def update(id, params)
+        perform_request_with_object(:put, "shipments/#{id}", params, Openlogi::Shipment)
+      end
+
+      def create(params)
+        perform_request_with_object(:post, "shipments", params, Openlogi::Shipment)
+      end
+
+      def destroy(id)
+        perform_request_with_object(:delete, "shipments/#{id}", {}, Openlogi::Shipment)
+      end
     end
   end
 end
--- lib/openlogi/api/warehousings.rb
+++ lib/openlogi/api/warehousings.rb
@@ -1,6 +1,25 @@
 module Openlogi
   module Api
     class Warehousings < Endpoint
+      def find(id)
+        perform_request_with_object(:get, "warehousings/#{id}", {}, Openlogi::Warehousing)
+      end
+
+      def all
+        perform_request_with_objects(:get, "warehousings", {}, Openlogi::Warehousing)
+      end
+
+      def update(id, params)
+        perform_request_with_object(:put, "warehousings/#{id}", params, Openlogi::Warehousing)
+      end
+
+      def create(params)
+        perform_request_with_object(:post, "warehousings", params, Openlogi::Warehousing)
+      end
+
+      def destroy(id)
+        perform_request_with_object(:delete, "warehousings/#{id}", {}, Openlogi::Warehousing)
+      end
     end
   end
 end
