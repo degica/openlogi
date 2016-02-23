dbd6a3c4254ec778ed3bf4d41e80c11e618622c5
Chris Salzberg <csalzberg@degica.com>
2016-02-23 11:51:50 +0900

    Add error properties

--- lib/openlogi/base_object.rb
+++ lib/openlogi/base_object.rb
@@ -2,5 +2,9 @@ module Openlogi
   class BaseObject < Hashie::Dash
     include Hashie::Extensions::Dash::Coercion
     include Hashie::Extensions::IndifferentAccess
+
+    property :error
+    property :errors
+    property :error_description
   end
 end
