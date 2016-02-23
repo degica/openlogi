9801ee45773ceb2eb50bfd2cd4a90bdfcccd6f16
Chris Salzberg <csalzberg@degica.com>
2016-02-23 15:50:04 +0900

    Add some missing properties on Stock

--- lib/openlogi/stock.rb
+++ lib/openlogi/stock.rb
@@ -8,5 +8,9 @@ module Openlogi
     property :quantity, coerce: Integer
     property :size, coerce: Enum[:SS, :S, :M, :L, :LL, :'3L']
     property :weight, coerce: Integer
+    property :backordered, coerce: Integer
+    property :created_at, coerce: DateTime
+    property :updated_at, coerce: DateTime
+    property :reserved, coerce: Integer
   end
 end
